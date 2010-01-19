0768043dd2d61df2137707a046a7aa3c89aeb294
Gianni Chiappetta <gianni@runlevel6.org>
2010-01-19 04:02:50 -0500

    Cleaner exit handling.

--- example/example.js
+++ example/example.js
@@ -5,25 +5,25 @@ var s = new Sandbox();
 
 // Example 1 - Standard JS
 s.run("1 + 1", function(output) {
-  sys.puts("Example 1: " + output);
+  sys.puts("Example 1: " + output + "\n");
 });
 
 // Example 2 - Something slightly more complex
 s.run("(function(name) { return 'Hi there, ' + name + '!'; })('Fabio')", function(output) {
-  sys.puts("Example 2: " + output);
+  sys.puts("Example 2: " + output + "\n");
 });
 
 // Example 3 - Syntax error
 s.run("lol)hai", function(output) {
-  sys.puts("Example 3: " + output);
+  sys.puts("Example 3: " + output + "\n");
 });
 
 // Example 4 - Restricted code
 s.run("process.platform", function(output) {
-  sys.puts("Example 4: " + output);
+  sys.puts("Example 4: " + output + "\n");
 });
 
 // Example 5 - Infinite loop
 s.run("while (true) {}", function(output) {
-  sys.puts("Example 5: " + output);
+  sys.puts("Example 5: " + output + "\n");
 });
