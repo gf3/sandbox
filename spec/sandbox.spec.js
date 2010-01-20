deaa8148c12a85c5dc37a413897af1b0b380757f
Gianni Chiappetta <gianni@runlevel6.org>
2010-01-20 00:54:33 -0500

    Added some basic specs. Added `simplicityjs` as a submodule.

---
+++ spec/sandbox.spec.js
@@ -0,0 +1,31 @@
+/* ------------------------------ INIT ------------------------------ */
+var mjsunit = require('mjsunit');
+process.mixin(GLOBAL, require('../vendor/simplicityjs/lib/simplicity'));
+process.mixin(GLOBAL, require('../lib/sandbox'));
+
+/* ------------------------------ Tests ------------------------------ */
+var sb = new Sandbox();
+
+expect('it should execute basic javascript', function(ok) {
+  sb.run('1 + 1', function(output) {
+    if (output === '2') ok();
+  });
+});
+
+expect('it should gracefully handle syntax errors', function(ok) {
+  sb.run('hi)there', function(output) {
+    if (output === 'SyntaxError: Unexpected token )') ok();
+  });
+});
+
+expect('it should effectively prevent code from accessing node', function(ok) {
+  sb.run('process.platform', function(output) {
+    if (output === "TypeError: Cannot read property 'platform' of null") ok();
+  });
+});
+
+expect('it should timeout on infinite loops', function(ok) {
+  sb.run('while (true) {}', function(output) {
+    if (output === "TimeoutError") ok();
+  });
+});
