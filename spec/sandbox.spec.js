77cb072081d24e49c680447ff5c24a10331c4900
Gianni Chiappetta <gianni@runlevel6.org>
2010-10-18 00:55:27 -0400

    updated: Removed dependancy. Fixed timeout bug.
    * Removed utools, use __proto__ instead of Object.prototype.extend.
    * Send proper signal (SIGHUP) to child process.
    * Set restricted vars to undefined rather than null.
    * Clean up some leakage (whoops).
    * Code style updates.
    * Updated tests.

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -1,45 +1,43 @@
 /* ------------------------------ INIT ------------------------------ */
-
-var Sandbox = require('../lib/sandbox')
+var Sandbox = require( '../lib/sandbox' )
+  , sb = new Sandbox()
 
 /* ------------------------------ Tests ------------------------------ */
-var sb = new Sandbox();
-exports['it should execute basic javascript'] = function(test) {
-  sb.run('1 + 1', function(output) {
-	test.equal(output,2)
-	test.finish();
-  });
-};
-
-exports['it should gracefully handle syntax errors'] = function(test) {
-  sb.run('hi)there', function(output) {
-	test.equal(output,"'SyntaxError: Unexpected token )'")
-	test.finish();
-  });
-};
-
-exports['it should effectively prevent code from accessing node'] = function(test) {
-  sb.run('process.platform', function(output) {
-	test.equal(output,"'TypeError: Cannot read property \\'platform\\' of null'")
-	test.finish();
-  });
-};
-
-exports['it should effectively prevent code from circumventing the sandbox'] = function(test) {
-  sb.run("1 + 1; sys= require('sys'); sys.puts('Up in your fridge'); function foo() {", function(output) {
-	test.equal(output,"'SyntaxError: Unexpected end of input'")
-	test.finish();
-  });
-};
-
-exports['it should timeout on infinite loops'] = function(test) {
-  sb.run('while (true) {}', function(output) {
-	test.equal(output,"TimeoutError")
-	test.finish();
-  });
-};
-
-if (module == require.main) {
-  require('async_testing').run(__filename, process.ARGV);
+exports['it should execute basic javascript'] = function( test ) {
+  sb.run( '1 + 1', function( output ) {
+    test.equal( output, '2' )
+    test.finish()
+  })
 }
+
+exports['it should gracefully handle syntax errors'] = function( test ) {
+  sb.run( 'hi )there', function( output ) {
+    test.equal( output, "'SyntaxError: Unexpected token )'" )
+    test.finish()
+  })
+}
+
+exports['it should effectively prevent code from accessing node'] = function( test ) {
+  sb.run( 'process.platform', function( output ) {
+    test.equal( output, "'TypeError: Cannot read property \\'platform\\' of undefined'" )
+    test.finish()
+  })
+}
+
+exports['it should effectively prevent code from circumventing the sandbox'] = function( test ) {
+  sb.run( "var sys=require('sys'); sys.puts('Up in your fridge')", function( output ) {
+    test.equal( output, "'TypeError: undefined is not a function'" )
+    test.finish()
+  })
+}
+
+exports['it should timeout on infinite loops'] = function( test ) {
+  sb.run( 'while ( true ) {}', function( output ) {
+    test.equal( output, "TimeoutError" )
+    test.finish()
+  })
+}
+
+if ( module == require.main )
+  require( 'async_testing' ).run( __filename, process.ARGV )
 
