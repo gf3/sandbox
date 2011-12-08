2286497843f36ab2289d482ee0af51c52d52475c
Florian Traverse <florian.traverse@gmail.com>
2011-12-08 10:29:01 +0100

    changed result api, so changed example accordingly

--- example/example.js
+++ example/example.js
@@ -2,27 +2,31 @@ var Sandbox = require("../lib/sandbox")
   , s = new Sandbox()
 
 // Example 1 - Standard JS
-s.run( "1 + 1", function( output ) {
-  console.log( "Example 1: " + output.result + "\n" )
+s.run( "1 + 1", function( err, result ) {
+  console.log( "Example 1: " + result + "\n" )
 })
-/*
+
 // Example 2 - Something slightly more complex
-s.run( "(function(name) { return 'Hi there, ' + name + '!'; })('Fabio')", function( output ) {
-  console.log( "Example 2: " + output.result + "\n" )
+s.run( "(function(name) { return 'Hi there, ' + name + '!'; })('Fabio')", function( err, result ) {
+  console.log( "Example 2: " + result + "\n" )
 })
 
 // Example 3 - Syntax error
-s.run( "lol)hai", function( output ) {
-  console.log( "Example 3: " + output.result + "\n" )
+s.run( "lol)hai", function( err, result ) {
+  console.log( "Example 3: " + result + "\n" )
 });
 
 // Example 4 - Restricted code
-s.run( "process.platform", function( output ) {
-  console.log( "Example 4: " + output.result + "\n" )
+s.run( "process.platform", function( err, result ) {
+  console.log( "Example 4: " + result + "\n" )
+})
+
+s.run( "console.log('hello, world')", function( err, result ) {
+  console.log( "Example 4: " + result + "\n" )
 })
 
 // Example 5 - Infinite loop
-s.run( "while (true) {}", function( output ) {
-  console.log( "Example 5: " + output.result + "\n" )
-})*/
+s.run( "while (true) {}", function( err, result ) {
+  console.log( "Example 5: " + result + "\n" )
+})
 
