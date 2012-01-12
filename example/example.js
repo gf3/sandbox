61f8a2f821d6bd0d03c463baf905c5622afaee7c
Florian Traverse <florian.traverse@gmail.com>
2012-01-12 03:23:45 +0100

    fixed a bunch of issues, added name to sandbox, tried to fix infinite loop vs timeout problem, improved event interface

--- example/example.js
+++ example/example.js
@@ -1,32 +1,71 @@
 var Sandbox = require("../lib/sandbox")
-  , s = new Sandbox()
-
+  , s = new Sandbox({name: "Example 1"})
+/**/
 // Example 1 - Standard JS
 s.run( "1 + 1", function( err, result ) {
-  console.log( "Example 1: " + result + "\n" )
+  if(err) return console.log( (s.name +" error:").bold.red, err )
+  console.log( s.name.bold.green, result )
 })
 
+s = new Sandbox({name: "Example 2"})
 // Example 2 - Something slightly more complex
 s.run( "(function(name) { return 'Hi there, ' + name + '!'; })('Fabio')", function( err, result ) {
-  console.log( "Example 2: " + result + "\n" )
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
 })
 
-// Example 3 - Syntax error
+s = new Sandbox({name: "Example 3"})
+// Example 3 - Plugin based api
+s.run( "console.log('hello, world')", function( err, result ) {
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
+})
+
+s = new Sandbox({name: "Example 4"})
+// Example 4 - Syntax error
 s.run( "lol)hai", function( err, result ) {
-  console.log( "Example 3: " + result + "\n" )
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
 });
 
-// Example 4 - Restricted code
+s = new Sandbox({name: "Example 5"})
+// Example 5 - Restricted code
 s.run( "process.platform", function( err, result ) {
-  console.log( "Example 4: " + result + "\n" )
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
 })
 
-s.run( "console.log('hello, world')", function( err, result ) {
-  console.log( "Example 4: " + result + "\n" )
+s = new Sandbox({name: "Example 6"})
+// Example 6 - Something more complex
+s.run(
+"function example(name) { throw Error('this is a dummy error '+ name || 'you') }\
+ (function toto() {example('Florian')})()",
+  function( err, result ) {
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
 })
 
-// Example 5 - Infinite loop
-s.run( "while (true) {}", function( err, result ) {
-  console.log( "Example 5: " + result + "\n" )
+
+s = new Sandbox({name: "Example 7"})
+// Example 7 - Long loop
+s.run( "for( var i=0; i<10000000; i++) {if(!(i%100000)) console.log('-',i/100000,'-')}", function( err, result ) {
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
 })
+
+
+s = new Sandbox({name: "Example 8"})
+// Example 8 - Using interval
+s.run( "setInterval(function(){console.log('-hello-')}, 50)", function( err, result ) {
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
+})
+/*
+s = new Sandbox({name: "Example 9"})
+// Example 9 - Infinite loop
+s.run( "while (true) {console.log('...')}", function( err, result ) {
+  if(err) return console.log( (s.name +" error:").bold.red, err.refinedStack )
+  console.log( s.name.bold.green, result )
+})
+*/
 
