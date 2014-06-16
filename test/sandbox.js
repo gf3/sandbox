2b04c78e0ac62173467f031c40be1fb5641a1d2d
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-06-16 15:05:59 -0700

    [FEATURE] IPC Messaging between Sandbox and shovel
    Sandbox.postMessage sends messages through to the sandboxed code
    User code can handle incoming messages by defining global `onmessage` function
    User code can send messages through to the outside of the sandbox by using global `postMessage` function
    Sandbox.on('message', function(message){...}) can be used to handle messages sent by sandboxed code
    Added tests, example, and documentation

--- test/sandbox.js
+++ test/sandbox.js
@@ -3,15 +3,18 @@
 //-----------------------------------------------------------------------------
 
 var should  = require('should');
+var sinon   = require('sinon');
 var Sandbox = require('../lib/sandbox');
-var sb      = new Sandbox();
-
 
 //-----------------------------------------------------------------------------
 // Tests
 //-----------------------------------------------------------------------------
 
 describe('Sandbox', function() {
+  var sb;
+  beforeEach(function(){
+    sb = new Sandbox();
+  });
 
   it('should execute basic javascript', function(done) {
     sb.run('1 + 1', function(output) {
@@ -70,6 +73,46 @@ describe('Sandbox', function() {
       output.console[0].should.eql('first');
       output.console[1].should.eql('second');
       done();
+    });
+  });
+
+  it('should expose the postMessage command to the sandboxed code', function(done){
+    var messageHandler = sinon.spy();
+    sb.on('message', messageHandler);
+    sb.run('postMessage("Hello World!");', function(output){
+      messageHandler.calledOnce.should.eql(true);
+      messageHandler.calledWith('Hello World!').should.eql(true);
+      done();
+    });
+  });
+
+  it('should allow sandboxed code to receive messages sent by postMessage from the outside by overwriting the onmessage function', function(done){
+    var messageHandler = sinon.spy();
+    sb.on('message', messageHandler);
+    sb.on('ready', function () {
+      sb.postMessage('Hello World!');
+    });
+    sb.run('onmessage = function (msg) { postMessage(msg); };', function(output) {
+      messageHandler.callCount.should.eql(1);
+      messageHandler.calledWith('Hello World!').should.eql(true);
+      done();
+    });
+  });
+  
+  it('should queue messages posted before the sandbox is ready and process them once it is', function(done){
+    var messageHandler = sinon.spy();
+    var num_messages_sent = 0;
+    var interval = setInterval(function(){
+      sb.postMessage(++num_messages_sent);
+    }, 1);
+    sb.on('message', messageHandler);
+    sb.run('onmessage = function (msg) { postMessage(msg); };', function(output) {
+      messageHandler.callCount.should.eql(num_messages_sent);
+      num_messages_sent.should.be.greaterThan(0);
+      done();
+    });
+    sb.on('ready', function(){
+      clearInterval(interval);
     });
   });
 
